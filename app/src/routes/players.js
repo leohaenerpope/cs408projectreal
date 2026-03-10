98b6e133c4243cc36cae97b9ddf0aaaae96fd7fd
Leo Haener-Pope <leohaenerpope@u.boisestate.edu>
2026-03-10 11:44:24 -0600

    Add player layout done

--- app/src/routes/players.js
+++ app/src/routes/players.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home (landing) page. */
+// GET player list page
 router.get('/', function(req, res, next) {
-  res.render('players', { title: 'NBA Player Matchup Notes - Player List' });
+	res.render('players', { title: 'NBA Player Matchup Notes - Player List' });
 });
 module.exports = router;
+
+// GET add player page
+router.get('/add', (req, res) => {
+	res.render('add-player', { title: 'Add New Player' });
+});
+  
+// POST for add player page
+router.post('/add', (req, res) => {
+	const { playerName } = req.body;
+	
+	console.log(`Saving: ${playerName}`);
+  
+	// TODO: SAVE TO DATABASE
+	
+	// After saving, send the user back to the list
+	res.redirect('/players');
+});
